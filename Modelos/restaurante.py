83c97a86207569f57eed4fd638f5e8da8c2657ab
Yuri Ernani <yuriernania@gmail.com>
2024-03-01 12:08:00 -0300

    Commit 03 - Funcionalidades Adicionadas

--- Modelos/restaurante.py
+++ Modelos/restaurante.py
@@ -1,4 +1,5 @@
 from Modelos.avaliacao import Avaliacao
+from Modelos.Cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
@@ -8,6 +9,7 @@ class Restaurante:
         self._categoria = categoria.title()
         self._ativo = False
         self._avaliacao = []
+        self._cardapio = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -43,3 +45,20 @@ class Restaurante:
         media = round(soma_das_notas / quantidade_de_notas, 1)
 
         return media
+
+    def adicionar_no_cardapio(self, item):
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do Restaurante {self._nome}\n')
+
+        for i, item in enumerate(self._cardapio, start = 1):
+            if hasattr(item, 'descricao'):
+                mensagem_prato = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+
+            else:
+                mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: {item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from Modelos.restaurante import Restaurante
+from Modelos.Cardapio.bebida import Bebida
+from Modelos.Cardapio.prato import Prato
 
 restaurante1 = Restaurante('Fogo Gaúcho Grill', 'Churrascaria Brasileira')
+bebida_suco = Bebida('Suco de Laranja', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
 
-restaurante1.receber_avaliacao('Yuri', 10)
-restaurante1.receber_avaliacao('Ana', 7)
-restaurante1.receber_avaliacao('Eduardo', 8)
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+
+restaurante1.adicionar_no_cardapio(bebida_suco)
+restaurante1.adicionar_no_cardapio(prato_paozinho)
 
 def exibir_nome_do_programa():
 
@@ -25,7 +31,7 @@ def exibir_nome_do_programa():
 
 def main():
     exibir_nome_do_programa()
-    Restaurante.listar_restaurantes()
+    restaurante1.exibir_cardapio
 
 if __name__ == '__main__':
     main()
